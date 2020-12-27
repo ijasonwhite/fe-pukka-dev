662c04b3237467a34b9e588ea4db616a49eccd46
Jason <jason@pukka.dev>
2020-12-27 17:35:44 +0000

    --

---
+++ src/pages/Services.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <h1>Services</h1>
+
+    <g-image src="https://i.inews.co.uk/content/uploads/2020/12/Screenshot-2020-12-01-at-09.47.45-640x360.png" width="500" />
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Services'
+  }
+}
+</script>
